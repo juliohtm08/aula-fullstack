d51baee17a45d89ec4171b51a0c63d972bccd211
juliohtm08 <jhtomoyuki345@gmail.com>
2025-04-23 12:39:49 -0300

    webpack-dev-server

--- Js/JS IV/Exercícios/Estruturando-projeto-moderno/webpack.config.js
+++ Js/JS IV/Exercícios/Estruturando-projeto-moderno/webpack.config.js
@@ -2,6 +2,13 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8000,
+  },
   entry: {
     index: './src/index.js',
   },
