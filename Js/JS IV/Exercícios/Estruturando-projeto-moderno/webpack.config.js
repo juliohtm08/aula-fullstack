b26cd2f344a8ea111b6d92a658def83baa03598d
juliohtm08 <jhtomoyuki345@gmail.com>
2025-04-23 12:12:10 -0300

    adicionando conteúdos faltantes

---
+++ Js/JS IV/Exercícios/Estruturando-projeto-moderno/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: 'defaults',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
