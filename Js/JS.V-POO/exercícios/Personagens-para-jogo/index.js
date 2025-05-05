da98a46d01731c642d23509fb3945c06d3629aad
juliohtm08 <jhtomoyuki345@gmail.com>
2025-05-05 20:32:32 -0300

    criação das classes

--- Js/JS.V-POO/exercícios/Personagens-para-jogo/index.js
+++ Js/JS.V-POO/exercícios/Personagens-para-jogo/index.js
@@ -9,7 +9,7 @@ const gale = new Mage('Gale', 95, 10, 0, 8);
 const laizel = new Warrior('Laizel', 100, 15, 10, 5);
 const warrior = new Warrior('warrior', 100, 15, 10, 5);
 
-console.log(mindFlayer);
+//console.log(mindFlayer);
 //console.log(asterion);
 //console.log(gale);
 //console.log(laizel);
@@ -31,6 +31,6 @@ console.log(mindFlayer); */
 //mindFlayer.attack(asterion);
 //console.log(asterion);
 
-console.log(asterion);
-gale.healLife(asterion);
-console.log(asterion);
+//console.log(asterion);
+//gale.healLife(asterion);
+//console.log(asterion);
