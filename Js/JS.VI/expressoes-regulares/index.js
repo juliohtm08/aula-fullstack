ebe464bbbaa85dbf0741fd44dfa0c5bccc4e7f9d
juliohtm08 <jhtomoyuki345@gmail.com>
2025-05-07 03:22:24 -0300

    add comentários ao código de expressões regulares

--- Js/JS.VI/expressoes-regulares/index.js
+++ Js/JS.VI/expressoes-regulares/index.js
@@ -1,9 +1,16 @@
 function PhoneNumber(phoneNumberString) {
-  const fixedString = phoneNumberString.replace(/[\sa-zA-z]/g, '');
-  this.countryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0];
-  this.ddd = fixedString.match(/(?<=\()\d+(?=\))/)[0];
-  this.number = fixedString.match(/(?<=\)).+/)[0];
-}
+  // Remove letras e espaços da string original
+  const fixedString = phoneNumberString.replace(/[\sa-zA-Z]/g, '');
 
+  // Captura o código do país (ex: +55, +1)
+  this.countryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0];
+
+  // Captura o DDD (número entre parênteses)
+  this.ddd = fixedString.match(/(?<=\()\d+(?=\))/)[0];
+
+  // Captura o restante do número, removendo o traço
+  this.number = fixedString.match(/(?<=\)).+/)[0].replace(/-/g, '');
+}
+console.log(new PhoneNumber('+55 (44) 9 1234-5678'));
 console.log(new PhoneNumber(' +aaa  +55 (22) 9 9876-5432'));
 console.log(new PhoneNumber('+1 (999) 9 as976-5432'));
